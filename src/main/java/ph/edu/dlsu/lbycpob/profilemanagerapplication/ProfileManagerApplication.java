72cf4471d7fc92f6a720d32c4b07335e2c50237f
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-15 19:14:34 +0800

    Added main to ProfileManagerApplication

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/ProfileManagerApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/ProfileManagerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfileManagerApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ProfileManagerApplication.class, args);
     }
-
 }
+
