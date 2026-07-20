20c20b04763558a7c21e3405b9d052471f07fb07
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-20 12:56:27 +0800

    ADded private and public field in Friend.java

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Friend.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Friend.java
@@ -19,3 +19,52 @@ import java.util.UUID;
  * friend_id   uuid not null, references profiles(id) on delete cascade
  * unique(profile_id, friend_id); check(profile_id <> friend_id)
  */
+
+@Entity
+@Table(name = "friends")
+public class Friend {
+
+    @Id
+    @UuidGenerator
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "profile_id", nullable = false, columnDefinition = "uuid")
+    private UUID profileId;
+
+    @Column(name = "friend_id", nullable = false, columnDefinition = "uuid")
+    private UUID friendId;
+
+    public Friend() {
+    }
+
+    public Friend(UUID id, UUID profileId, UUID friendId) {
+        this.id = id;
+        this.profileId = profileId;
+        this.friendId = friendId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(UUID profileId) {
+        this.profileId = profileId;
+    }
+
+    public UUID getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(UUID friendId) {
+        this.friendId = friendId;
+    }
+
