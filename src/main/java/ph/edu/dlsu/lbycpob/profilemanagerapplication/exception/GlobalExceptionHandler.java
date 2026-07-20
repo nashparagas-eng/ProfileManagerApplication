beb12626cac1a3655643eddb087114b9f86ece65
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-20 12:54:45 +0800

    added imports to GlobalExceptionHandler.java

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/exception/GlobalExceptionHandler.java
@@ -1,4 +1,10 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.exception;
 
-public class GlobalExceptionHandler {
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.dto.Dtos.ApiError;
+
+import java.util.NoSuchElementException;
