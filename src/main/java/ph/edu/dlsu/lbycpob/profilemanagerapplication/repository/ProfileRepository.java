19f16e7b633ab9ab3f70a028f467541e69a6f8b1
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-20 12:45:52 +0800

    Fixed import name

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/ProfileRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/ProfileRepository.java
@@ -1,8 +1,8 @@
-package ph.edu.dlsu.lbycpob.profilemanager.repository;
+package ph.edu.dlsu.lbycpob.profilemanagerapplication.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Profile;
 
 import java.util.List;
 import java.util.Optional;
