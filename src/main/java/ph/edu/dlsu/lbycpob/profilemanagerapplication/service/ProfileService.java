91a5733b1ccbc41c3f7ce990a4653b2c5aa0590b
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-20 12:58:33 +0800

    Added imports to ProfileService.java

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/service/ProfileService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/service/ProfileService.java
@@ -1,4 +1,14 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.service;
 
-public class ProfileService {
-}
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Friend;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Profile;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.repository.FriendRepository;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.repository.ProfileRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
