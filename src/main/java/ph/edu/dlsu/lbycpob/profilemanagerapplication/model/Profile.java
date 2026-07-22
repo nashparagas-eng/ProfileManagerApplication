213109303c309bdd46f028579539a265ed5aa702
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:31:56 +0800

    Add getQuote and setQuote methods

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -83,4 +83,12 @@ public class Profile {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
 }
