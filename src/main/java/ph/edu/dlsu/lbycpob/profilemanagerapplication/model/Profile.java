7f44cb013128afb3e61838ff63e63102d25cac7e
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:32:28 +0800

    Add getPicture and setPicture methods

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -91,4 +91,12 @@ public class Profile {
     public void setQuote(String quote) {
         this.quote = quote;
     }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
