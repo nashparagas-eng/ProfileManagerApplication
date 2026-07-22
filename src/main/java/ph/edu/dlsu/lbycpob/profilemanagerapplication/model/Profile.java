403f9426be795321a1fc738bb4c73886ca53f2ce
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:20:01 +0800

    Import Table annotation for defining table name

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -3,3 +3,4 @@ package ph.edu.dlsu.lbycpob.profilemanager.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
