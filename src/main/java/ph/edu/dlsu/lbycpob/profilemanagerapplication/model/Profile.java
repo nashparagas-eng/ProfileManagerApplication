a02b35423914ee14b29ea3fa5bfe56599500a3ff
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:20:33 +0800

    Import UuidGenerator annotation for auto-generating IDs

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -4,3 +4,4 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.UuidGenerator;
