e75467dc72870cf4fd81fd8a83277c573a965f7f
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:21:09 +0800

    Import OffsetDateTime, Objects, and UUID standard utilities

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -5,3 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.UuidGenerator;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
