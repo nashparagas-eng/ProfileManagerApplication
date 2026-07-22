c3c650f34e3b31321fb2e6742aea843e3f07b8cf
Tin <francis_martin_ang@dlsu.edu.ph>
2026-07-22 19:54:15 +0800

    pasted all imports and package

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/service/ImageCommissionService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/service/ImageCommissionService.java
@@ -1,4 +1,16 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.service;
 
-public class ImageCommissionService {
-}
+import com.luciad.imageio.webp.WebPWriteParam;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
