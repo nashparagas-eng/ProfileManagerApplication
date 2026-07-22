ebf61f627f3fb92c749d20b3055764ee23f14d02
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-22 20:31:28 +0800

    Add getStatus and setStatus methods

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/model/Profile.java
@@ -75,4 +75,12 @@ public class Profile {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
